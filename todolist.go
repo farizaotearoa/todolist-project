b0045851eb96d0c8cdbfff3160e2adce4560714b
farizaotearoa <decoractionjr@gmail.com>
2023-03-07 16:40:53 +0700

    Create new task function

--- todolist.go
+++ todolist.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
@@ -14,9 +16,13 @@ var dsn = "root:password@tcp(0.0.0.0:3306)/todolist?charset=utf8&parseTime=True&
 var db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 type TodoItemModel struct {
-	Id     int `gorm:"primary_key"`
-	Task   string
-	Finish bool
+	Id          int `gorm:"primary_key"`
+	Task        string
+	LimitedTime bool
+	Deadline    time.Time
+	Finish      bool
+	Overdue     bool
+	Urgent      bool
 }
 
 func main() {
@@ -25,6 +31,7 @@ func main() {
 	log.Info("Starting To do list API Server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
+	router.HandleFunc("/create", createTask).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
 
@@ -37,4 +44,23 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var todoReq TodoItemModel
+	reqBody, _ := io.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &todoReq)
+
+	log.WithFields(log.Fields{"task": todoReq.Task}).Info("Add new Task. Saving to database")
+	deadline := todoReq.Deadline
+	limitedTime := true
+	if deadline.IsZero() {
+		deadline = time.Now()
+		limitedTime = false
+	}
+	todo := &TodoItemModel{Task: todoReq.Task, LimitedTime: limitedTime, Deadline: deadline, Finish: false, Overdue: false, Urgent: todoReq.Urgent}
+	db.Create(&todo)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&todo)
 }
