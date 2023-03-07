35c3d5f359ccf680caceea19ff23b386288ea6ae
farizaotearoa <decoractionjr@gmail.com>
2023-03-07 17:33:23 +0700

    Add update & delete function

--- todolist.go
+++ todolist.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -31,7 +32,9 @@ func main() {
 	log.Info("Starting To do list API Server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
-	router.HandleFunc("/create", createTask).Methods("POST")
+	router.HandleFunc("/todo/create", CreateTask).Methods("POST")
+	router.HandleFunc("/todo/update/{id}", UpdateTask).Methods("PUT")
+	router.HandleFunc("/todo/delete/{id}", DeleteTask).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
 
@@ -46,12 +49,11 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, `{"alive": true}`)
 }
 
-func createTask(w http.ResponseWriter, r *http.Request) {
-	var todoReq TodoItemModel
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	todoReq := &TodoItemModel{}
 	reqBody, _ := io.ReadAll(r.Body)
 	json.Unmarshal(reqBody, &todoReq)
 
-	log.WithFields(log.Fields{"task": todoReq.Task}).Info("Add new Task. Saving to database")
 	deadline := todoReq.Deadline
 	limitedTime := true
 	if deadline.IsZero() {
@@ -61,6 +63,47 @@ func createTask(w http.ResponseWriter, r *http.Request) {
 	todo := &TodoItemModel{Task: todoReq.Task, LimitedTime: limitedTime, Deadline: deadline, Finish: false, Overdue: false, Urgent: todoReq.Urgent}
 	db.Create(&todo)
 
+	log.WithFields(log.Fields{"task": todoReq.Task}).Info("Add new Task")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(&todo)
+}
+
+func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	todo := &TodoItemModel{}
+	result := db.First(&todo, id)
+	if result.Error != nil {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"error": "Record Not Found"}`)
+	} else {
+		finish, _ := strconv.ParseBool(r.FormValue("finish"))
+		log.WithFields(log.Fields{"Id": id, "Task": todo.Task, "Finish": finish}).Info("Updating Task")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		todo.Finish = finish
+		db.Save(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(&todo)
+	}
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	todo := &TodoItemModel{}
+	result := db.First(&todo, id)
+	if result.Error != nil {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"error": "Record Not Found"}`)
+	} else {
+		log.WithFields(log.Fields{"Id": id, "Task": todo.Task}).Info("Deleting Task")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		db.Delete(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{}`)
+	}
 }
